cfd28b78153ce688587f635bbb4e912a1bc7aa8c
Egor Dubrovskiy <egor.dubrovskiy.1998@mail.ru>
2022-02-21 07:42:15 +0300

    Add test code from Introduction section

---
+++ app/code/EgorDubrovskiy/EnterprisePatterns/Model/DevelopmentPrinciples/NameFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EgorDubrovskiy\EnterprisePatterns\Model\DevelopmentPrinciples;
+
+class NameFactory
+{
+    public function nameFactory(): void
+    {
+        // TODO: Implement nameFactory() method.
+    }
+
+    public function getName(string $name): Namer
+    {
+        // TODO: Implement getName() method.
+    }
+}
